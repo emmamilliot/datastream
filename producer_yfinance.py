032d67d7e5765ce3bcec0d4e7199c77968ac87aa
bergamote91 <ecole399@gmail.com>
2022-03-20 22:28:04 +0100

    #pretraining using 5days past data

--- producer_yfinance.py
+++ producer_yfinance.py
@@ -6,7 +6,12 @@ import pandas as pd
 import requests as req
 # https://pypi.org/project/yfinance/
 import yfinance as yf
+from river.stream import iter_pandas
 
+def construction_dataset(data_):
+  X = data_.iloc[:-1,:]
+  y = data_['Close'].iloc[1:]
+  return list(iter_pandas(X=X, y=y))
 
 #Function to get the data from Yahoo finance API
 def get_data(time_step):
@@ -15,10 +20,10 @@ def get_data(time_step):
       data = yf.Ticker(ticker_name)
 
       #Dans le cas où nous faisons la requête pour la première fois, pré-training du modèle avec toutes les valeurs de la journée
-      #Cas à faire (non fonctionnel pour le moment)
       if time_step==0:
-        hist = data.history(period='1d', interval='1m')
-        return hist
+        hist = data.history(period='5d', interval='1m')
+        
+        return {i:dict(list(elem.items())+list({'y_true':y}.items())) for i,(elem,y) in enumerate(construction_dataset(hist))}
       #Online learning : on prend la donnée en temps réelle, dernière donnée disponible
       else : 
         hist = pd.DataFrame(data.history(period='1d', interval='1m').iloc[-1])
@@ -27,7 +32,9 @@ def get_data(time_step):
         #from datetime import datetime
         #dt_object = datetime.fromtimestamp(timestamp)
         hist = hist.rename(columns={curr_dt : str(timestamp)})
-        return hist.to_dict()
+        hist = hist.to_dict()
+        hist[str(timestamp)].update({'y_true':None})
+        return hist
 
     except:
         print("Debug code")
@@ -35,7 +42,7 @@ def get_data(time_step):
 #Function to publish a message
 def publish_message(producerkey,key,data_key):
     try:
-        key_bytes = bytes(key, encoding='utf-8')
+        key_bytes = bytes(str(key), encoding='utf-8')
         producerkey.send("yfinanceapi", json.dumps(data[key]).encode('utf-8'), key_bytes)
         print('message_published')
     except:
@@ -50,16 +57,12 @@ def kafka_producer_connection():
         print("Connection error")
 
 #Declearing main function
-i=1
+i=0
 while True:
     data = get_data(i)
-    # print(data)
-    # print(len(data))
     if len(data) > 0:
-        # print(data)
         kafka_producer = kafka_producer_connection()
-        # print(len(data))
         for i,key in enumerate(sorted(data)):
             publish_message(kafka_producer,key, data[key])
-            time.sleep(60)
     i+=1
+    time.sleep(60)
